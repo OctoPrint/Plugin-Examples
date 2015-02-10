900f6b973103c37d126fc3fb0c6f0452e580be04
Gina Häußge <osd@foosel.net>
2015-02-10 16:24:28 +0100

    Updated helloworld example plugin to match tutorial

--- helloworld/octoprint_helloworld/__init__.py
+++ helloworld/octoprint_helloworld/__init__.py
@@ -5,7 +5,8 @@ import octoprint.plugin
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.SettingsPlugin):
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.AssetPlugin):
 	def on_after_startup(self):
 		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
 
@@ -17,6 +18,13 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
 			dict(type="navbar", custom_bindings=False),
 			dict(type="settings", custom_bindings=False)
 		]
+
+	def get_assets(self):
+		return dict(
+			js=["js/helloworld.js"],
+			css=["css/helloworld.css"],
+			less=["less/helloworld.less"]
+		)
 
 __plugin_name__ = "Hello World"
 __plugin_implementations__ = [HelloWorldPlugin()]
--- helloworld/octoprint_helloworld/static/js/helloworld.js
+++ helloworld/octoprint_helloworld/static/js/helloworld.js
@@ -2,13 +2,27 @@ $(function() {
     function HelloWorldViewModel(parameters) {
         var self = this;
 
-        // We want to only display some of the components when the user is logged in, so we need the login state view
-        // view model from the application
         self.loginState = parameters[0];
+        self.settings = parameters[1];
 
-        // This is just some custom value that we have bound to at a couple of spans in the template files which
-        // normally read "inactive"
-        self.someValue = ko.observable("active");
+        // this will hold the URL currently displayed by the iframe
+        self.currentUrl = ko.observable();
+
+        // this will hold the URL entered in the text field
+        self.newUrl = ko.observable();
+
+        // this will be called when the user clicks the "Go" button and set the iframe's URL to the entered URL
+        self.goToUrl = function() {
+            self.currentUrl(self.newUrl());
+        };
+
+        // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its depedencies have
+        // already been initialized. It is especially guaranteed that this method gets called _after_ the settings
+        // have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
+        self.onBeforeBinding = function() {
+            self.newUrl(self.settings.settings.plugins.helloworld.url());
+            self.goToUrl();
+        }
     }
 
     // This is how our plugin registers itself with the application, by adding some configuration information to
@@ -20,18 +34,9 @@ $(function() {
         // This is a list of dependencies to inject into the plugin, the order which you request here is the order
         // in which the dependencies will be injected into your view model upon instantiation via the parameters
         // argument
-        ["loginStateViewModel"],
+        ["loginStateViewModel", "settingsViewModel"],
 
-        // Finally, this is the list of all elements we want this view model to be bound to. We see all our components
-        // here that we defined in get_template_configs of our TemplatePlugin implementation, using the default IDs
-        // assigned to them by OctoPrint
-        [
-            document.getElementById("tab_plugin_helloworld"),
-            document.getElementById("tab_plugin_helloworld_link"),
-            document.getElementById("navbar_plugin_helloworld"),
-            document.getElementById("sidebar_plugin_helloworld_wrapper"),
-            document.getElementById("settings_plugin_helloworld"),
-            document.getElementById("settings_plugin_helloworld_2")
-        ]
+        // Finally, this is the list of all elements we want this view model to be bound to.
+        [document.getElementById("tab_plugin_helloworld")]
     ]);
 });
