26efc2d110d52299994ca929e6ede68e348b5e99
Gina Häußge <osd@foosel.net>
2015-10-29 16:39:00 +0100

    Example for octoprint.cli.commands hook

---
+++ custom_cli_command.py
@@ -0,0 +1,37 @@
+import click
+
+def clitest_commands(*args, **kwargs):
+	@click.command("greet")
+	@click.option("--greeting", "-g", default="Hello", help="The greeting to use")
+	@click.argument("name", default="World")
+	def greet_command(greeting, name):
+		"""Greet someone by name, the greeting can be customized."""
+		click.echo("{} {}!".format(greeting, name))
+
+
+	@click.command("random")
+	@click.argument("name", default="World")
+	@click.pass_context
+	def random_greet_command(ctx, name):
+		"""Greet someone by name with a random greeting."""
+
+		greetings = [
+			"Hello",
+			"Buon giorno",
+			"Hola",
+			"Konnichiwa",
+			"Oh hai",
+			"Hey",
+			"Salve"
+		]
+
+		from random import randrange
+		greeting = greetings[randrange(0, len(greetings))]
+		ctx.invoke(greet_command, greeting=greeting, name=name)
+
+	return [greet_command, random_greet_command]
+
+
+__plugin_hooks__ = {
+	"octoprint.cli.commands": clitest_commands
+}
