318c6e6a8828da096bf1a7b84b68eade0a51b923
Gina Häußge <osd@foosel.net>
2015-12-08 13:36:27 +0100

    Fixed tornado route example

--- add_tornado_route.py
+++ add_tornado_route.py
@@ -5,13 +5,13 @@ import octoprint.plugin
 class TornadoRoutePlugin(octoprint.plugin.SettingsPlugin):
 
 	def route_hook(self, server_routes, *args, **kwargs):
-		import os
 		from octoprint.server.util.tornado import LargeResponseHandler, UrlProxyHandler, path_validation_factory
+		from octoprint.util import is_hidden_path
 
 		return [
 			(r"/download/(.*)", LargeResponseHandler, dict(path=self._settings.global_get_basefolder("uploads"),
 			                                               as_attachment=True,
-			                                               path_validation=path_validation_factory(lambda path: not os.path.basename(path).startswith("."),
+			                                               path_validation=path_validation_factory(lambda path: not is_hidden_path(path),
 			                                                                                       status_code=404))),
 			(r"forward", UrlProxyHandler, dict(url=self._settings.global_get(["webcam", "snapshot"]),
 			                                        as_attachment=True))
