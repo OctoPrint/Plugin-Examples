16785089674c0b5e1f2193fcf4d66abe19404570
Gina Häußge <osd@foosel.net>
2016-03-24 12:08:56 +0100

    Use logger instead of print in received hook example

--- read_m115_response.py
+++ read_m115_response.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import logging
+
 def detect_machine_type(comm, line, *args, **kwargs):
 	if "MACHINE_TYPE" not in line:
 		return line
@@ -7,9 +9,11 @@ def detect_machine_type(comm, line, *args, **kwargs):
 	# Create a dict with all the keys/values returned by the M115 request
 	printer_data = dict(d.split(':') for d in line.split(' '))
 	
-	print "Machine type detected: {machine}.".format(machine=printer_data["MACHINE_TYPE"])
+	logging.getLogger("octoprint.plugin." + __name__).info("Machine type detected: {machine}.".format(machine=printer_data["MACHINE_TYPE"]))
 
 	return line
 
 __plugin_name__ = "Detect Machine Data"
-__plugin_hooks__ = {"octoprint.comm.protocol.gcode.recieved": detect_machine_type}
+__plugin_hooks__ = {
+	"octoprint.comm.protocol.gcode.recieved": detect_machine_type
+}
