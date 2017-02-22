173b48bfd8f686bdc298d4fa5bf11da9affeec39
Gina Häußge <gina@octoprint.org>
2017-02-22 17:35:26 +0100

    Fix example to not hiccup on : or whitespace at wrong location
    Closes #2

--- read_m115_response.py
+++ read_m115_response.py
@@ -6,9 +6,11 @@ def detect_machine_type(comm, line, *args, **kwargs):
 	if "MACHINE_TYPE" not in line:
 		return line
 
+	from octoprint.util.comm import parse_firmware_line
+
 	# Create a dict with all the keys/values returned by the M115 request
-	printer_data = dict(d.split(':') for d in line.split(' '))
-	
+	printer_data = parse_firmware_line(line)
+
 	logging.getLogger("octoprint.plugin." + __name__).info("Machine type detected: {machine}.".format(machine=printer_data["MACHINE_TYPE"]))
 
 	return line
