bd88fc80e1300251b9867c1b85538a176e0fc541
mscalora <mike@scalora.org>
2022-01-31 07:08:26 -0700

    Update strip_comments to work correctly in Python 3

--- strip_all_comments.py
+++ strip_all_comments.py
@@ -8,10 +8,10 @@ from octoprint.util.comm import strip_comment
 
 class CommentStripper(octoprint.filemanager.util.LineProcessorStream):
 	def process_line(self, line):
-		line = strip_comment(line).strip()
-		if not len(line):
+		decoded_line = strip_comment(line.decode()).strip()
+		if not len(decoded_line):
 			return None
-		return line + "\r\n"
+		return (decoded_line + "\r\n").encode()
 
 def strip_all_comments(path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
 	if not octoprint.filemanager.valid_file_type(path, type="gcode"):
@@ -27,7 +27,7 @@ def strip_all_comments(path, file_object, links=None, printer_profile=None, allo
 __plugin_name__ = "Strip comments from GCODE"
 __plugin_description__ = "Strips all comments and empty lines from uploaded/generated GCODE files ending on the name " \
                          "postfix \"_strip\", e.g. \"some_file_strip.gcode\"."
-__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_pythoncompat__ = ">=3,<4"
 __plugin_hooks__ = {
 	"octoprint.filemanager.preprocessor": strip_all_comments
 }
